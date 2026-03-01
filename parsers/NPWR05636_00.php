39201d3b57ed88d5c0de63471fc17616f2beebbb
FlexBy420 <flexby420@gmail.com>
2026-03-01 13:29:56 +0100

    Update NPWR05636_00.php

--- parsers/NPWR05636_00.php
+++ parsers/NPWR05636_00.php
@@ -6,10 +6,24 @@ return [
         "game_id" => ["BLES01976", "BLUS31426", "NPJB00549"],
         "score_boards" => [1,2,3,4],
         "names" => [
-            1 => "Travelling Distance (Peaceful)",
-            2 => "Travelling Distance (Easy)",
-            3 => "Travelling Distance (Normal)",
-            4 => "Travelling Distance (Hard)"
+            1 => "Travelling Distance | Peaceful",
+            2 => "Travelling Distance | Easy",
+            3 => "Travelling Distance | Normal",
+            4 => "Travelling Distance | Hard",
+
+            #5 => "Mining Blocks | Peaceful",
+            #6 => "Mining Blocks | Easy",
+            #7 => "Mining Blocks | Normal",
+            #8 => "Mining Blocks | Hard",
+
+            #9 => "Farming | Peaceful",
+            #10 => "Farming | Easy",
+            #11 => "Farming | Normal",
+            #12 => "Farming | Hard",
+
+            #13 => "Kills | Easy",
+            #14 => "Kills | Normal",
+            #15 => "Kills | Hard",
         ]
     ],
     // not completed, only displays distance as total
